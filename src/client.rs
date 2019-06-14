e1714961bd2893c1c910ce986fc2bdfe151938cf
satellitex <s.a.t.e.3.ths@gmail.com>
2019-06-14 17:08:52 +0900

    WIP: wip

---
+++ src/client.rs
@@ -0,0 +1,51 @@
+use super::*;
+use futures::Future;
+use jsonrpc_core::Result as RpcResult;
+use jsonrpc_core_client::transports::ws;
+use jsonrpc_core_client::{RpcChannel, RpcError, TypedClient};
+use jsonrpc_pubsub::{typed::Subscriber, SubscriptionId};
+use parity_codec::Decode;
+use sr_rpc::chain::*;
+use tokio;
+
+use std::cell::RefCell;
+
+pub type Result<T> = std::result::Result<T, jsonrpc_core_client::RpcError>;
+
+pub struct SubstrateClient<Number, Hash, Header, SignedBlock> {
+    runtime: tokio::runtime::Runtime,
+    chain_client: RefCell<ChainClient<Number, Hash, Header, SignedBlock>>,
+}
+
+impl<Number, Hash, Header, SignedBlock> SubstrateClient<Number, Hash, Header, SignedBlock>
+where
+    Hash: serde::de::DeserializeOwned
+        + serde::ser::Serialize
+        + std::marker::Send
+        + std::marker::Sync
+        + serde::de::DeserializeOwned,
+    Header: std::marker::Send + std::marker::Sync + serde::de::DeserializeOwned,
+    Number: serde::ser::Serialize + std::marker::Send + std::marker::Sync,
+    SignedBlock: std::marker::Send + std::marker::Sync + serde::de::DeserializeOwned,
+{
+    /// Get hash of the n-th block in the canon chain.
+    ///
+    /// By default returns latest block hash.
+    pub fn latest_block_hash(&self) -> Result<Option<Hash>> {
+        self.chain_client.borrow_mut().block_hash(None).wait()
+    }
+
+    pub fn new(uri: &str) -> Result<Self> {
+        let mut runtime = tokio::runtime::Runtime::new()?;
+        let chain_client = runtime.block_on(ws::connect::<ChainClient<Number, Hash, Header, SignedBlock>>(uri)?)?;
+
+        Ok(SubstrateClient {
+            runtime: runtime,
+            chain_client: RefCell::new(chain_client),
+        })
+    }
+
+    pub fn shutdown(self) -> std::result::Result<(), ()> {
+        self.runtime.shutdown_now().wait()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,22 @@
+use futures::Future;
 use jsonrpc_core_client::transports::ws;
-use jsonrpc_core_client::{RpcError, TypedClient, RpcChannel};
+use jsonrpc_core_client::{RpcChannel, RpcError, TypedClient};
 use metadata::RuntimeMetadataPrefixed;
 use parity_codec::Decode;
-use sr_primitives::{H256, Bytes};
+use sr_primitives::generic;
+use substrate_primitives::{Bytes, H256};
 use std::time::Duration;
-use futures::Future;
-use tokio;
 
-#[derive(Clone)]
-struct SubstrateClient(TypedClient);
+use node_primitives::{BlockNumber, Hash};
+use node_runtime::{Header, Block};
 
-impl From<RpcChannel> for SubstrateClient {
-    fn from(channel: RpcChannel) -> Self {
-        SubstrateClient(channel.into())
-    }
-}
+mod client;
 
-impl SubstrateClient {
-    fn get_block_hash(&self) -> impl Future<Item = H256, Error = RpcError> {
-        self.0.call_method("chain_getBlockHash", "H256", ())
-    }
-    fn get_metadata(&self, msg: H256) -> impl Future<Item = Bytes, Error = RpcError> {
-        self.0.call_method("state_getMetadata", "Bytes", (msg,))
-    }
-}
+const LOCAL_URL: &str = "ws://127.0.0.1:9944";
+
 
 fn main() {
-    let (tx, rx) = std::sync::mpsc::channel();
-
-    // Connect to the url and call the closure
-    let call = ws::connect("ws://127.0.0.1:9944")
-        .expect("expected client")
-        .and_then(|client: SubstrateClient| {
-            client.get_block_hash().then(move |res| {
-                let _ = tx.send(res);
-                Ok(())
-            })
-        })
-        .map_err(|e| panic!("RPC Client error: {:?}", e));
-
-    tokio::run(call);
-
-    // then
-    let res = rx.recv_timeout(Duration::from_secs(3)).unwrap();
-
-    if let Err(RpcError::Other(err)) = res {
-        panic!("Expected a tokio::Error {:?}", err)
-    } else {
-        println!("Expected JsonRpcError. Received {:?}", res)
-    }
-
-    let (tx, rx) = std::sync::mpsc::channel();
-    let call = ws::connect("ws://127.0.0.1:9944")
-        .expect("expected client")
-        .and_then(|client: SubstrateClient| {
-            client.get_metadata(res.unwrap()).then(move |res| {
-                let _ = tx.send(res);
-                Ok(())
-            })
-        })
-        .map_err(|e| panic!("RPC Client error: {:?}", e));
-
-    tokio::run(call);
-
-    // then
-    let res = rx.recv_timeout(Duration::from_secs(3)).unwrap();
-
-    if let Ok(binary) = res {
-        let metadata = RuntimeMetadataPrefixed::decode(&mut &(*binary)).expect("unk3");
-        println!("Expected JsonRpcError. Received Metadata: {:?}.", metadata);
-    } else {
-        panic!("Expected a Error {:?}", res)
-    }
+    let client = client::SubstrateClient::<BlockNumber, Hash, Header, generic::SignedBlock<Block>>::new(LOCAL_URL).unwrap();
+    let hash = client.latest_block_hash();
+    println!("Get Hash: {:?}", hash);
 }
